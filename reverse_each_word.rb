357fee342e037b809127d05725e0498da5c53f1a
julio hernandez <julioandres72@gmail.com>
2020-10-20 14:12:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,17 @@
+def reverse_each_word_with_each(string)
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do |string|
+    return_array << string.reverse_each_word
+  end
+  return_array.join(" ")
+end
+
+
+
+
+
+
 def reverse_each_word(string)
   array = string.split(" ")
   test_array = []
@@ -7,4 +21,3 @@ def reverse_each_word(string)
   test_array.join(" ")
 end
 
-reverse_each_word("Hello there, and how are you?")
